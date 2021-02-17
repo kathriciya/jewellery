6e4f12987b5e1de0c0487bf27a22386c69eb246c
Ekaterina <Kata_Yurevna@mail.ru>
2021-02-17 22:49:31 +0700

    Добивала login.js

--- source/js/login.js
+++ source/js/login.js
@@ -1,26 +1,6 @@
 'use strict';
 
 (function () {
-  // var list = document.querySelector('faq__list');
-
-  // if (list) {
-  //   list.classList.remove('faq__list--nojs');
-
-  //   var toggle = function (item) {
-  //     item.classList.toggle('faq__item--show');
-  //   };
-
-  //   list.addEventListener('click', function (evt) {
-  //     var item = evt.target.closest('li');
-  //     toggle(item);
-  //   });
-  // }
-
-})();
-
-'use strict';
-
-(function () {
   var OUT = 27;
   var button = document.querySelector('.header__header-login .header-login');
   var login = document.querySelector('.login');
@@ -62,29 +42,5 @@
       login.classList.remove('modal--show');
     }
   });
-
-})();
-
-'use strict';
-
-// мобильное меню - начиная с планшетной версии
-
-(function () {
-  var header = document.querySelector('.header');
-  var toggle = document.querySelector('.header__toggle');
-
-  if (header && toggle) {
-    header.classList.remove('header--nojs');
-
-    toggle.addEventListener('click', function () {
-      if (header.classList.contains('header--closed')) {
-        header.classList.remove('header--closed');
-        header.classList.add('header--opened');
-      } else {
-        header.classList.add('header--closed');
-        header.classList.remove('header--opened');
-      }
-    });
-  }
 
 })();
